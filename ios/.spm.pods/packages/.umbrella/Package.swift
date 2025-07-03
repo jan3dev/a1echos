b63e0f3dc0d8f7ae10175ea1c695e3c785ecada0
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2025-07-02 20:12:16 -0600

    chore: bump whisperkit version

--- ios/.spm.pods/packages/.umbrella/Package.swift
+++ ios/.spm.pods/packages/.umbrella/Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 let package = Package(
   name: "_umbrella_",
   dependencies: [
-    .package(url: "https://github.com/argmaxinc/WhisperKit.git", .upToNextMajor(from: "0.13.0"))
+    .package(url: "https://github.com/argmaxinc/WhisperKit.git", exact: "0.13.0")
   ]
 )
